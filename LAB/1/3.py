dee7500b93dfef6fcb74670b036970e28581a22f
Daniel Stoyanov <65760410+danielchobest@users.noreply.github.com>
2024-05-15 20:24:25 +0300

    Fundamentals module Python SoftUni Project

--- LAB/1/3.py
+++ LAB/1/3.py
@@ -1,4 +1,10 @@
-word = input()
+# word = input()
+#
+# for i in range(len(word) -1, -1, -1):
+#     print(word[i], end='')
 
-for i in range(len(word) -1, -1, -1):
-    print(word[i], end='')
+word = input()
+print(word[::-1])
+
+# Python Slice Notation
+# https://sentry.io/answers/python-slice-notation/
