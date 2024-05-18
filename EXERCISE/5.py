36c0a99654de52c9d51c3c13e46e2fad488014e5
Daniel Stoyanov <65760410+danielchobest@users.noreply.github.com>
2024-05-18 11:28:31 +0300

    Softuni Fundamentals Module Homework

--- EXERCISE/5.py
+++ EXERCISE/5.py
@@ -3,8 +3,14 @@ prices = 0
 
 for _ in range(n):
     price = float(input())
+    if 0.01 > price > 100.00:
+        continue
     days = int(input())
+    if 1 > days > 31:
+        continue
     capsules = int(input())
+    if 1 > capsules > 2000:
+        continue
     prices += price*capsules*days
     print(f"The price for the coffee is: ${price*capsules*days:0.02f}")
 
