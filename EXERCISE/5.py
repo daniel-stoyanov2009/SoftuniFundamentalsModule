b1a3a95807677df8d6dc3e4a558f0cc6afbed6d5
Daniel Stoyanov <65760410+danielchobest@users.noreply.github.com>
2024-05-18 11:16:54 +0300

    New Commit

--- EXERCISE/5.py
+++ EXERCISE/5.py
@@ -1,3 +1,11 @@
 n = int(input())
+prices = 0
 
-for _ in range()
+for _ in range(n):
+    price = float(input())
+    days = int(input())
+    capsules = int(input())
+    prices += price*capsules*days
+    print(f"The price for the coffee is: ${price*capsules*days:0.02f}")
+
+print(f"Total: ${prices:0.02f}")
