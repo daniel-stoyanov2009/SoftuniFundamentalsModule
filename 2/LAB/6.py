91447b87451804acc41fe6d74ccb2be0c8bae5d5
Daniel Stoyanov <65760410+danielchobest@users.noreply.github.com>
2024-05-23 18:40:23 +0300

    Softuni Fundamentals Module Homework

--- 2/LAB/6.py
+++ 2/LAB/6.py
@@ -1,16 +1,6 @@
-year = int(input())
+year = int(input()) + 1
 
-while True:
-    if len(str(year)) == 1:
-        year_string = f"000{year}"
-    elif len(str(year)) == 2:
-        year_string = f"00{year}"
-    elif len(str(year)) == 3:
-        year_string = f"0{year}"
-    else:
-        year_string = f"{year}"
-    check = {year_string[0], year_string[1], year_string[2], year_string[3]}
-    if len(check) == 4:
-        print(year_string)
-        break
+while len(set(str(year))) != len(str(year)):
     year += 1
+
+print(year)
